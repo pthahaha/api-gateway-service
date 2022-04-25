5180ff2595a5e8cf94982933c01e1d5433168267
teri.epi <teri.epi@kakaoenterprise.com>
2022-04-25 18:52:43 +0900

    msa - api gateway service

---
+++ src/main/java/kr/pe/acet/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.acet.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
+
+}
